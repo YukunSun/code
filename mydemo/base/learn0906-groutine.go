cd8ed591c1ac638517797ba1bf14b0b8eca5a400
sunyk <sunyukun@shopin.cn>
2019-09-06 07:38:24 +0800

    race candition

---
+++ mydemo/base/learn0906-groutine.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	//声明一个共享资源，供多个 groutine 同时读写
+	counter int
+	wg      sync.WaitGroup
+)
+
+func main() {
+	wg.Add(2)
+	go increaseCounter(1)
+	go increaseCounter(2)
+
+	wg.Wait()
+	fmt.Println("result :", counter) //result : 2
+}
+
+func increaseCounter(i int) {
+	defer wg.Done()
+	for i := 0; i < 2; i++ {
+		value := i
+		//类比 java 的 Thread.join()
+		runtime.Gosched()
+		value++
+		counter = value
+	}
+}
