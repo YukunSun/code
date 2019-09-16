bb71195c59c8fa7669b7312840e9eb11715dd968
sunyk <sunyukun@shopin.cn>
2019-09-16 08:07:13 +0800

    mutual exclusion

---
+++ mydemo/base/learn0916-mutex.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var (
+	counter int
+	wg      sync.WaitGroup
+	mutex   sync.Mutex
+)
+
+func main() {
+	wg.Add(2)
+
+	go incCounter()
+	go incCounter()
+
+	wg.Wait()
+	fmt.Printf("result counter:%d \n", counter)
+}
+
+func incCounter() {
+	defer wg.Done()
+	for i := 0; i < 2; i++ {
+		mutex.Lock()
+		{
+			value := counter
+			//退出当前 goroutine，给其他 goroutine 执行的机会。类比 java 的 Thread.join()
+			runtime.Gosched()
+			value++
+			counter = value
+		}
+		mutex.Unlock()
+	}
+}
